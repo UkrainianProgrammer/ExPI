6963fd4f4fd88839a3e473693e9ad9220c92640b
Alex S <insten490@gmail.com>
2025-01-31 19:53:21 -0600

    added middleware handlers

--- src/middleware/common.ts
+++ src/middleware/common.ts
@@ -5,3 +5,12 @@ import compression from "compression";
 
 export const handleCors = (router: Router) => 
     router.use(cors({ credentials: true, origin: true }))
+
+export const handleBodyParsing = (router: Router) => {
+    router.use(parser.urlencoded({ extended: true }));
+    router.use(parser.json());
+}
+
+export const handleCompression = (router: Router) => {
+    router.use(compression());
+}
