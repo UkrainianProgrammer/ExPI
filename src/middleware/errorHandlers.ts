d360068390a0ff69a0371f502307e36c53f19e66
Alex S <insten490@gmail.com>
2025-02-03 22:21:22 -0600

    finished error handlers

--- src/middleware/errorHandlers.ts
+++ src/middleware/errorHandlers.ts
@@ -6,3 +6,26 @@ const handle404Error = (router: Router) => {
         throw new HTTP404Error("Page not found");
     });
 };
+
+const handleClientError = (router: Router) => {
+    router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+        if (err instanceof HTTPClientError) {
+            res.status(err.status).send({ error: err.message });
+        } else {
+            next(err);
+        }
+    });
+}
+
+const handleServerErrors = (router: Router) => {
+    router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+        console.log(err);
+        if (process.env.NODE_ENV !== 'production') {
+            res.status(500).send({ error: "Internal Server Error" });
+        } else {
+            res.status(500).send(err.stack);
+        }
+    });
+}
+
+export default [handle404Error, handleClientError, handleServerErrors];
