87748b3d616576d97296b50c82cfda7725ac715c
Alex S <insten490@gmail.com>
2025-02-05 20:43:56 -0600

    implemented applyRoutes method

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -14,3 +14,10 @@ export const applyMiddleware = (
 type Handler = (req: Request, res: Response, next: NextFunction) => Promise<void> | void
 
 type Route = {path: string, method: string, handler: Handler | Handler[]}
+
+export const applyRoutes = (routes: Route[], router: Router) => {
+    for (const route of routes) {
+        const { method, path, handler } = route;
+        (router as any)[method](path, handler);
+    }
+}
